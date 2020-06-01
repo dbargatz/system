8c6c57322b3dc30dade5c28a884382a19536bcd2
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-06-01 00:32:58 +0000

    Silence compilation steps.

--- src/kernel/Makefile.mk
+++ src/kernel/Makefile.mk
@@ -8,12 +8,12 @@ KERNEL               = $(KERNEL_BUILD_DIR)/kernel.bin
 
 $(KERNEL) : $(KERNEL_ASM_SRC) $(KERNEL_CPP_SRC)
 	@mkdir -p $(KERNEL_BUILD_DIR)
-	for s in $(KERNEL_CPP_SRC); do \
+	@for s in $(KERNEL_CPP_SRC); do \
 		$(CXX) -c $(CPPFLAGS) $(CXXFLAGS) -o $(KERNEL_BUILD_DIR)/$$(basename $$s .cpp).o $$s; \
 		done
-	for s in $(KERNEL_ASM_SRC); do \
+	@for s in $(KERNEL_ASM_SRC); do \
 		$(AS) $(ASFLAGS) -o $(KERNEL_BUILD_DIR)/$$(basename $$s .asm).o $$s; \
 		done
 	@rm -rf $(KERNEL_OBJ) # Prevents 'File truncated' error when linking
-	$(LD) -r -o $(KERNEL_OBJ) $(KERNEL_BUILD_DIR)/*.o
-	$(LD) -n --gc-sections -T $(KERNEL_LD_SRC) $(KERNEL_OBJ) -o $@
+	@$(LD) -r -o $(KERNEL_OBJ) $(KERNEL_BUILD_DIR)/*.o
+	@$(LD) -n --gc-sections -T $(KERNEL_LD_SRC) $(KERNEL_OBJ) -o $@
