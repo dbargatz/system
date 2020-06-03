b51654f69600c101f984ec9b1e961a5a04a7b78c
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-06-03 01:53:30 +0000

    Reduce logging, add TODO.

--- src/driver/cpu/x64/pic.cpp
+++ src/driver/cpu/x64/pic.cpp
@@ -19,7 +19,6 @@ void PIC::remap(const uint8_t in_pic1_interrupt_base,
     // Save off the current interrupt masks.
     uint8_t pic1_mask = inb(PIC1_DATA_PORT);
     uint8_t pic2_mask = inb(PIC2_DATA_PORT);
-    gLog.debug("Original masks: PIC1({#02X}) PIC2({#02X})\n", inb(PIC1_DATA_PORT), inb(PIC2_DATA_PORT));
 
     // Start the initialization of both PICs. After this command is sent,
     // each PIC will expect three data bytes in sequence on their data port:
@@ -33,7 +32,6 @@ void PIC::remap(const uint8_t in_pic1_interrupt_base,
     // Send ICW2, the interrupt vector base.
     outb(PIC1_DATA_PORT, in_pic1_interrupt_base);
     outb(PIC2_DATA_PORT, in_pic2_interrupt_base);
-    gLog.debug("Set interrupt bases: PIC1({#02X}) PIC2({#02X})\n", in_pic1_interrupt_base, in_pic2_interrupt_base);
 
     // Send ICW3, the cascaded PIC identity.
     outb(PIC1_DATA_PORT, PIC2_IRQ_NUMBER << 2);
@@ -44,15 +42,16 @@ void PIC::remap(const uint8_t in_pic1_interrupt_base,
     outb(PIC2_DATA_PORT, CPU_MODE_8086);
 
     // Restore the saved interrupt masks.
+    // TODO: Is this necessary? We don't change the mask above, does init wipe out the mask? Check datasheet.
     outb(PIC1_DATA_PORT, pic1_mask);
     outb(PIC2_DATA_PORT, pic2_mask);
-    gLog.debug("Current masks: PIC1({#02X}) PIC2({#02X})\n", inb(PIC1_DATA_PORT), inb(PIC2_DATA_PORT));
 }
 
 void PIC::disable_all(void) {
     // Mask all interrupts on both the PICs.
     outb(PIC2_DATA_PORT, 0xFF);
     outb(PIC1_DATA_PORT, 0xFF);
+    gLog.debug("Disabled all IRQs\n");
 }
 
 void PIC::disable_irq(const uint8_t in_irq_number) {
@@ -66,8 +65,7 @@ void PIC::disable_irq(const uint8_t in_irq_number) {
         mask = inb(PIC2_DATA_PORT) | (1 << (in_irq_number - 8));
         outb(PIC2_DATA_PORT, mask);
     }
-    gLog.debug("Disabled IRQ {#02X}\n", in_irq_number);
-    gLog.debug("Current masks: PIC1({#02X}) PIC2({#02X})\n", inb(PIC1_DATA_PORT), inb(PIC2_DATA_PORT));
+    gLog.debug("Disabled IRQ {#02X} ({})\n", in_irq_number, in_irq_number);
 }
 
 void PIC::enable_irq(const uint8_t in_irq_number) {
@@ -81,6 +79,5 @@ void PIC::enable_irq(const uint8_t in_irq_number) {
         mask = inb(PIC2_DATA_PORT) & ~(1 << (in_irq_number - 8));
         outb(PIC2_DATA_PORT, mask);
     }
-    gLog.debug("Enabled IRQ {#02X}\n", in_irq_number);
-    gLog.debug("Current masks: PIC1({#02X}) PIC2({#02X})\n", inb(PIC1_DATA_PORT), inb(PIC2_DATA_PORT));
+    gLog.debug("Enabled IRQ {#02X} ({})\n", in_irq_number, in_irq_number);
 }
