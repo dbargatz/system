9ce0a0c1fada61e81da51a052a63daf57b33d83d
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-06-10 12:57:30 +0000

    Clean up stdint.h format.

--- src/driver/cpu/x64/std/stdint.h
+++ src/driver/cpu/x64/std/stdint.h
@@ -1,19 +1,18 @@
-#ifndef STDINT
-#define STDINT 1
+#ifndef _STD_STDINT_H
+#define _STD_STDINT_H
 
-typedef signed char int8_t;
-typedef signed short int16_t;
-typedef signed int int32_t;
-typedef signed long long int64_t;
+typedef signed char         int8_t;
+typedef signed short        int16_t;
+typedef signed int          int32_t;
+typedef signed long long    int64_t;
 
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
+typedef unsigned char       uint8_t;
+typedef unsigned short      uint16_t;
+typedef unsigned int        uint32_t;
+typedef unsigned long long  uint64_t;
+typedef uint64_t            size_t;
 
-typedef float float32_t;
-typedef double float64_t;
+typedef float               float32_t;
+typedef double              float64_t;
 
-typedef uint64_t size_t;
-
-#endif //STDINT
+#endif // _STD_STDINT_H
