2df5ef77a93c19a5d225270324f04bedbf19ecd6
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-06-10 13:22:50 +0000

    Migrate linker script to driver structure.

--- src/kernel/Makefile.mk
+++ src/kernel/Makefile.mk
@@ -6,9 +6,10 @@ KERNEL_BUILD_DIR     = $(BUILD_DIR)/kernel
 KERNEL_OBJ           = $(KERNEL_BUILD_DIR)/kernel.o
 KERNEL               = $(KERNEL_BUILD_DIR)/kernel.bin
 
-DRIVER_SRC_DIR       = $(SRC_DIR)/driver
+DRIVER_SRC_DIR       = $(SRC_DIR)/driver/cpu/x64
 DRIVER_ASM_SRC       = $(shell find $(DRIVER_SRC_DIR) -name *.asm)
 DRIVER_CPP_SRC       = $(shell find $(DRIVER_SRC_DIR) -name *.cpp)
+DRIVER_LD_SRC        = $(DRIVER_SRC_DIR)/linker.ld
 
 $(KERNEL) : $(KERNEL_ASM_SRC) $(KERNEL_CPP_SRC) $(DRIVER_ASM_SRC) $(DRIVER_CPP_SRC)
 	@mkdir -p $(KERNEL_BUILD_DIR)
@@ -26,4 +27,4 @@ $(KERNEL) : $(KERNEL_ASM_SRC) $(KERNEL_CPP_SRC) $(DRIVER_ASM_SRC) $(DRIVER_CPP_S
 		done
 	@rm -rf $(KERNEL_OBJ) # Prevents 'File truncated' error when linking
 	@$(LD) -r -o $(KERNEL_OBJ) $(KERNEL_BUILD_DIR)/*.o
-	@$(LD) -n --gc-sections -T $(KERNEL_LD_SRC) $(KERNEL_OBJ) -o $@
+	@$(LD) -n --gc-sections -T $(DRIVER_LD_SRC) $(KERNEL_OBJ) -o $@
