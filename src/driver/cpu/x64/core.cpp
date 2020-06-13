4263538e656a0bea2a1d432d9595dec3bc23ef59
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-06-13 16:51:50 +0000

    Add PIT to Core.

--- src/driver/cpu/x64/core.cpp
+++ src/driver/cpu/x64/core.cpp
@@ -3,15 +3,13 @@
 #include "std/panic.h"
 
 Core::Core(logger& in_log, const void * in_boot_info,
-    InterruptManager& in_interrupts) : 
-    _log(in_log), _boot_info(in_boot_info), _interrupts(in_interrupts) {
-        // Register the default panic handler.
-        //_interrupts.register_handler(InterruptType::PANIC, panic_handler);
-
+    InterruptManager& in_interrupts, PIT& in_pit) : 
+    _log(in_log), _boot_info(in_boot_info), _interrupts(in_interrupts), 
+    _pit(in_pit) {
         _log.debug("Constructed Core:\n");
         _log.debug("    Boot Info    : {#016X}\n", (uint64_t)_boot_info);
         _log.debug("    Interrupts   : {}abled\n", _interrupts.enabled() ? "en" : "dis");
-        //_log.debug("    Panic Handler: {#016X}\n", (uint64_t)panic_handler);
+        _log.debug("    PIT          : present\n");
         _log.debug("    Serial Port  : present\n");
 }
 
@@ -24,6 +22,16 @@ void Core::run() {
     asm volatile("int $150\n");
     asm volatile("int $254\n");
     asm volatile("int $255\n");
-    _log.info("Done with software interrupts, panicking!\n");
+
+    _log.info("Done with software interrupts, testing timer! Interrupt state: {}\n", _interrupts.enabled() ? "enabled" : "disabled");
+    _pit.set_frequency(50);
+    _interrupts.temp_unmask(0);
+    _interrupts.enable_interrupts();
+    uint32_t ct = 0xFFFFFFFF;
+    while(ct > 0) {
+        ct--;
+    }
+
+    _log.info("Done with timer, panicking!\n");
     PANIC("oh bother");
 }
--- src/driver/cpu/x64/core.hpp
+++ src/driver/cpu/x64/core.hpp
@@ -3,17 +3,20 @@
 
 #include "std/logger.hpp"
 #include "interrupts/interrupt_manager.hpp"
+#include "timer/pit.hpp"
 
 class Core {
 private:
     logger& _log;
     const void * _boot_info;
     InterruptManager& _interrupts;
+    PIT& _pit;
 
 public:
     Core(logger& in_log,
         const void * in_boot_info,
-        InterruptManager& in_interrupts);
+        InterruptManager& in_interrupts,
+        PIT& in_pit);
 
     void run();
 };
