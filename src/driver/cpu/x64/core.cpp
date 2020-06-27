f221c11bd1784caf3b08061557349a28d90d3226
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-06-27 00:46:27 +0000

    Add ASSERT() macro; use in Core.

--- src/driver/cpu/x64/core.cpp
+++ src/driver/cpu/x64/core.cpp
@@ -1,25 +1,23 @@
 #include "core.hpp"
-#include "std/halt.h"
-#include "std/panic.h"
-#include "timer/pit.hpp"
+#include "std/assert.h"
 
 Core::Core(logger& in_log, const void * in_boot_info,
     InterruptManager& in_interrupts, ITimer& in_timer, keyboard& in_kbd) :
      _log(in_log), _boot_info(in_boot_info), timer(in_timer),
      interrupts(in_interrupts), kbd(in_kbd) {
-        _log.debug("Constructed Core:\n");
-        _log.debug("    Boot Info    : {#016X}\n", (uint64_t)_boot_info);
-        _log.debug("    Interrupts   : {}abled\n", interrupts.enabled() ? "en" : "dis");
-        _log.debug("    PIT          : present\n");
-        _log.debug("    Serial Port  : present\n");
-        _log.debug("    Keyboard     : present\n");
+        _log.debug("Constructed Core:");
+        _log.debug("    Boot Info    : {#016X}", (uint64_t)_boot_info);
+        _log.debug("    Interrupts   : {}abled", interrupts.enabled() ? "en" : "dis");
+        _log.debug("    PIT          : present");
+        _log.debug("    Serial Port  : present");
+        _log.debug("    Keyboard     : present");
 }
 
 void Core::run() {
     kbd.reset();
     interrupts.enable_interrupts();
     interrupts.temp_unmask(1);
-    _log.info("Testing keyboard (interrupts: {}abled)...\n", interrupts.enabled() ? "en" : "dis");
+    _log.info("Testing keyboard (interrupts: {}abled)...", interrupts.enabled() ? "en" : "dis");
     uint32_t ct = 0xFFFFFFFF;
     while(ct > 0) {
         ct--;
@@ -27,12 +25,12 @@ void Core::run() {
 
     timer.set_frequency(1000.0);
     interrupts.temp_unmask(0);
-    _log.info("Testing timer (interrupts: {}abled)...\n", interrupts.enabled() ? "en" : "dis");
+    _log.info("Testing timer (interrupts: {}abled)...", interrupts.enabled() ? "en" : "dis");
     ct = 0xFFFFFFFF;
     while(ct > 0) {
         ct--;
     }
 
-    _log.info("Testing panic (will halt)...\n");
-    PANIC("oh bother");
+    _log.info("Testing ASSERT and PANIC (will halt)...");
+     ASSERT(0 == 1, "oh bother");
 }
