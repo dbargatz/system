78082fcf1a487cad961dff721feb1ed28537e016
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-06-30 00:50:31 +0000

    Fix memcpy off-by-one error.

--- src/driver/cpu/x64/std/memcpy.cpp
+++ src/driver/cpu/x64/std/memcpy.cpp
@@ -5,7 +5,7 @@ void * memcpy(void * in_destination, const void * in_source, size_t in_num) {
     // TODO: ensure in_destination/in_source are valid
     uint8_t * dest = (uint8_t *)in_destination;
     uint8_t * src = (uint8_t *)in_source;
-    while(in_num >= 0) {
+    while(in_num > 0) {
         *dest++ = *src++;
         in_num--;
     }
