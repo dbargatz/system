89002e823bb724fb6ae9e450d61d1eece1ef75db
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-07-03 02:38:43 +0000

    Shrink GDT dump logging.

--- src/driver/cpu/x64/interrupts/gdt.cpp
+++ src/driver/cpu/x64/interrupts/gdt.cpp
@@ -5,7 +5,7 @@ extern struct _gdt gdt64;
 gdt::gdt(logger& in_log) : _log(in_log), _our_gdt(*(struct _gdt *)&gdt64) { }
 
 void gdt::dump() {
-    _log.debug("GDT           : {#016X}", (uint64_t)&gdt64);
+    _log.debug("GDT: {#016X}", (uint64_t)&gdt64);
     for(auto& entry : _our_gdt.entries) {
         _dump_entry(entry);
     }
@@ -35,26 +35,25 @@ void gdt::install(uint8_t in_index, const void * in_base, uint32_t in_limit,
 }
 
 void gdt::_dump_entry(const struct _gdt_entry& in_entry) {
-    auto base = in_entry.base_0_15 + (in_entry.base_16_23 << 16) + (in_entry.base_24_31 << 24);
+    auto entry_idx = (uint16_t)((uint8_t*)&in_entry - (uint8_t*)&_our_gdt);
     auto limit = in_entry.limit_0_15 + (in_entry.limit_16_19 << 16);
-    bool is_code = in_entry.executable;
-    _log.debug("\t{#04X}:", (uint16_t)((uint8_t*)&in_entry - (uint8_t*)&_our_gdt));
-    _log.debug("\t\tBase            : {#016X}", (uint64_t)base);
-    _log.debug("\t\tLimit           : {#016X}", (uint32_t)limit);
-    _log.debug("\t\tAccessed        : {}", in_entry.accessed ? "Yes" : "No");
-    if(is_code) {
-        _log.debug("\t\tRead-Accessible : {}", in_entry.read_write ? "Readable" : "Unreadable");
-        _log.debug("\t\tConforming      : {}", in_entry.dir_conform ? "Yes" : "No");
-    } else {
-        _log.debug("\t\tRead/Write      : {}", in_entry.read_write ? "R/W" : "Read-Only");
-        _log.debug("\t\tDirection       : {}", in_entry.dir_conform ? "Down" : "Up");
-    }
+    auto base = in_entry.base_0_15  +
+        (in_entry.base_16_23 << 16) +
+        (in_entry.base_24_31 << 24);
 
-    _log.debug("\t\tExecutable      : {}", in_entry.executable ? "Yes" : "No");
-    _log.debug("\t\tDescriptor Type : {}", in_entry.descriptor_type ? "Code/Data" : "System");
-    _log.debug("\t\tPrivilege Level : {}", (uint8_t)in_entry.priv_level);
-    _log.debug("\t\tPresent         : {}", in_entry.present ? "Yes" : "No");
-    _log.debug("\t\t64bit Code Seg  : {}", in_entry.is_64bit_cs ? "Yes" : "No");
-    _log.debug("\t\tSize Bit        : {}", in_entry.size_bit ? "Set" : "Clear");
-    _log.debug("\t\tGranularity     : {}", in_entry.size_bit ? "4KiB" : "1 byte");
+    uint8_t access = in_entry.accessed  +
+        (in_entry.read_write      << 1) +
+        (in_entry.dir_conform     << 2) +
+        (in_entry.executable      << 3) +
+        (in_entry.descriptor_type << 4) +
+        (in_entry.priv_level      << 5) +
+        (in_entry.present         << 7);
+
+    uint8_t flags = in_entry.reserved +
+        (in_entry.is_64bit_cs << 1)   +
+        (in_entry.size_bit    << 2)   +
+        (in_entry.granularity << 3);
+
+    _log.debug("\t{#04X}: {016X}:{04X} Access: {02X} Flags: {01X}", entry_idx,
+        (uint64_t)base, (uint32_t)limit, access, flags);
 }
