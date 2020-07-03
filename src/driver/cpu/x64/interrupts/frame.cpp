4a2ea2fb9c8a8527a122b47a974a3ce53d9f0995
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-07-03 01:19:54 +0000

    Make formatting more A E S T H E T I C.

--- src/driver/cpu/x64/interrupts/frame.cpp
+++ src/driver/cpu/x64/interrupts/frame.cpp
@@ -4,15 +4,21 @@ interrupt_frame::interrupt_frame(logger& in_log, const void * in_frame_ptr) :
     _log(in_log), frame((struct _interrupt_frame *)in_frame_ptr) { }
 
 void interrupt_frame::dump() {
-    _log.debug("Registers:");
-    _log.debug("\tcs : {#016X}\trip: {#016X}\tss : {#016X}\trsp: {#016X}",
-               frame->cs, frame->rip, frame->ss, frame->rsp);
-    _log.debug("\trax: {#016X}\trbx: {#016X}\trcx: {#016X}\trdx: {#016X}",
-               frame->rax, frame->rbx, frame->rcx, frame->rdx);
-    _log.debug("\trdi: {#016X}\trsi: {#016X}\tr8 : {#016X}\tr9 : {#016X}",
-               frame->rdi, frame->rsi, frame->r8, frame->r9);
-    _log.debug("\tr10: {#016X}\tr11: {#016X}\tr12: {#016X}\tr13: {#016X}",
-               frame->r10, frame->r11, frame->r12, frame->r13);
-    _log.debug("\tr14: {#016X}\tr15: {#016X}", frame->r14, frame->r15);
-    _log.debug("\trflags: {#016X}", frame->rflags);
+    _log.panic("Interrupt:");
+    _log.panic("\tvector : {#02X} ({})", frame->interrupt_number,
+        frame->interrupt_number);
+    _log.panic("\terror  : {#04X} ({})", frame->error_code, frame->error_code);
+    _log.panic("Registers:");
+    _log.panic("\trflags : {016X}", frame->rflags);
+    _log.panic("\tcs:rip : {04X}:{016X}", frame->cs, frame->rip);
+    _log.panic("\tss:rsp : {04X}:{016X}", frame->ss, frame->rsp);
+    _log.panic("\trax: {016X}\trbx: {016X}\trcx: {016X}", frame->rax, 
+        frame->rbx, frame->rcx);
+    _log.panic("\trdx: {016X}\trdi: {016X}\trsi: {016X}", frame->rdx,
+        frame->rdi, frame->rsi);
+    _log.panic("\tr8 : {016X}\tr9 : {016X}\tr10: {016X}", frame->r8, frame->r9,
+        frame->r10);
+    _log.panic("\tr11: {016X}\tr12: {016X}\tr13: {016X}", frame->r11,
+        frame->r12, frame->r13);
+    _log.panic("\tr14: {016X}\tr15: {016X}", frame->r14, frame->r15);
 }
