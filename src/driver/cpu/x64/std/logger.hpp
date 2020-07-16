41c39da786a82cf6e2627f60c3336d1f95cd8acb
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-07-16 13:03:14 +0000

    Pass logger_backends via member function instead of ctor.

--- src/driver/cpu/x64/std/logger.hpp
+++ src/driver/cpu/x64/std/logger.hpp
@@ -1,7 +1,7 @@
 #ifndef _STD_LOGGER_HPP
 #define _STD_LOGGER_HPP
 
-#include "array.hpp"
+#include "assert.h"
 #include "logger_backend.hpp"
 #include "text.hpp"
 
@@ -9,19 +9,17 @@ class logger {
 public:
     enum class level : uint8_t { Debug, Info, Warn, Error, Panic };
 
-    logger(std::array<logger_backend*,2> in_backends) : _backends{in_backends[0], in_backends[1]} {
-        auto be1 = in_backends[0];
-        auto be2 = in_backends[1];
-        be1->debug(text("BE0: {X}", (uint64_t)be1).get());
-        be2->debug(text("BE1: {X}", (uint64_t)be2).get());
+    void add_backend(logger_backend* in_backend) {
+        ASSERT(_num_backends < _MAX_BACKENDS, "too many logger backends");
+        _backends[_num_backends++] = in_backend;
     }
 
     template<typename ... Args>
     void debug(const text& in_fmt, Args&&... in_args) {
         text msg(_MSG_FORMAT, _LEVEL_SYMBOLS[(uint8_t)level::Debug], in_fmt.get());
         text final_msg(msg.get(), static_cast<Args>(in_args)...);
-        for(auto be : _backends) {
-            be->debug(final_msg);
+        for(auto i = 0; i < _num_backends; i++) {
+            _backends[i]->debug(final_msg);
         }
     }
 
@@ -29,8 +27,8 @@ public:
     void error(const text& in_fmt, Args&&... in_args) {
         text msg(_MSG_FORMAT, _LEVEL_SYMBOLS[(uint8_t)level::Error], in_fmt.get());
         text final_msg(msg.get(), static_cast<Args>(in_args)...);
-        for(auto be : _backends) {
-            be->error(final_msg);
+        for(auto i = 0; i < _num_backends; i++) {
+            _backends[i]->error(final_msg);
         }
     }
 
@@ -41,8 +39,8 @@ public:
     void info(const text& in_fmt, Args&&... in_args) {
         text msg(_MSG_FORMAT, _LEVEL_SYMBOLS[(uint8_t)level::Info], in_fmt.get());
         text final_msg(msg.get(), static_cast<Args>(in_args)...);
-        for(auto be : _backends) {
-            be->info(final_msg);
+        for(auto i = 0; i < _num_backends; i++) {
+            _backends[i]->info(final_msg);
         }
     }
 
@@ -50,8 +48,8 @@ public:
     void panic(const text& in_fmt, Args&&... in_args) {
         text msg(_MSG_FORMAT, _LEVEL_SYMBOLS[(uint8_t)level::Panic], in_fmt.get());
         text final_msg(msg.get(), static_cast<Args>(in_args)...);
-        for(auto be : _backends) {
-            be->panic(final_msg);
+        for(auto i = 0; i < _num_backends; i++) {
+            _backends[i]->panic(final_msg);
         }
     }
 
@@ -59,8 +57,8 @@ public:
     void warn(const text& in_fmt, Args&&... in_args) {
         text msg(_MSG_FORMAT, _LEVEL_SYMBOLS[(uint8_t)level::Warn], in_fmt.get());
         text final_msg(msg.get(), static_cast<Args>(in_args)...);
-        for(auto be : _backends) {
-            be->warn(final_msg);
+        for(auto i = 0; i < _num_backends; i++) {
+            _backends[i]->warn(final_msg);
         }
     }
 
@@ -74,9 +72,11 @@ private:
     };
 
     static constexpr char _HEXDUMP_FORMAT[] = "[{}] {}{#016X}:\t";
-    static constexpr char _MSG_FORMAT[] = "[{}] {}\n";
+    static constexpr char _MSG_FORMAT[]     = "[{}] {}\n";
+    static constexpr uint8_t _MAX_BACKENDS  = 2;
 
-    logger_backend* _backends[2];
+    logger_backend* _backends[_MAX_BACKENDS] = {0};
+    uint8_t _num_backends = 0;
 };
 
 #endif // _STD_LOGGER_HPP
