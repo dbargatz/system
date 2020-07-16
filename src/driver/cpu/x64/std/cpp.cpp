23d94dcfca4cfae953938eb654fc9c4fa26f44e2
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-07-16 13:02:35 +0000

    Add __cxa_atexit() and machinery for atexit() dtors.

--- src/driver/cpu/x64/std/cpp.cpp
+++ src/driver/cpu/x64/std/cpp.cpp
@@ -1,6 +1,32 @@
 #include "cpp.hpp"
+#include "assert.h"
 #include "panic.h"
+
+struct atexit_entry {
+    void (*func)(void *);
+    void *arg;
+    void *dso_handle;
+};
+
+constexpr uint8_t ATEXIT_MAX_FUNCS = 128;
+
+void * __dso_handle = nullptr;
+uint8_t __num_atexit_funcs = 0;
+struct atexit_entry __atexit_funcs[ATEXIT_MAX_FUNCS] = {{0}};
 
 extern "C" void __cxa_pure_virtual() {
     PANIC("Pure virtual function called!");
+}
+
+extern "C" int __cxa_atexit(void (*in_func)(void *), void * in_arg, void * in_dso_handle) {
+    if(__num_atexit_funcs >= ATEXIT_MAX_FUNCS) {
+        return -1;
+    }
+
+    __atexit_funcs[__num_atexit_funcs].func = in_func;
+    __atexit_funcs[__num_atexit_funcs].arg = in_arg;
+    __atexit_funcs[__num_atexit_funcs].dso_handle = in_dso_handle;
+    __num_atexit_funcs++;
+
+    return 0;
 }
