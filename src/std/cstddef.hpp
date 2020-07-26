6a2a910063c232e35508c822fe87e3193e0f66b1
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-07-26 22:04:41 +0000

    Update cstddef with ptrdiff_t, max_align_t, offsetof, and NULL.

--- src/std/cstddef.hpp
+++ src/std/cstddef.hpp
@@ -6,13 +6,17 @@
 #ifndef _STD_CSTDDEF_HPP
 #define _STD_CSTDDEF_HPP
 
+#include <cstdint.hpp>
+
 namespace std {
-    using nullptr_t = decltype(nullptr);
-    using size_t = unsigned long long;
-    // TODO: ptrdiff_t
+    using max_align_t = long double;
+    using nullptr_t   = decltype(nullptr);
+    using ptrdiff_t   = int64_t;
+    using size_t      = uint64_t;
     // TODO: byte
-    // TODO: max_align_t
-    // TODO: offsetof()
 }; //namespace std
+
+#define offsetof(st, m) __builtin_offsetof(st, m);
+#define NULL ((void *)0)
 
 #endif // _STD_CSTDDEF_HPP
