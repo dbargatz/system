b8981a7dc1d3021b81a4ff0c7b6c436d166bcc5e
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-07-26 22:03:58 +0000

    Create the <cstdint.hpp> header.

---
+++ src/std/cstdint.hpp
@@ -0,0 +1,38 @@
+#ifndef _STD_CSTDINT_HPP
+#define _STD_CSTDINT_HPP
+
+namespace std {
+
+using int8_t         = signed char;
+using int16_t        = signed short;
+using int32_t        = signed int;
+using int64_t        = signed long long;
+using int_fast8_t    = signed char;
+using int_fast16_t   = signed short;
+using int_fast32_t   = signed int;
+using int_fast64_t   = signed long long;
+using int_least8_t   = signed char;
+using int_least16_t  = signed short;
+using int_least32_t  = signed int;
+using int_least64_t  = signed long long;
+using intmax_t       = signed long long;
+using intptr_t       = signed long long;
+
+using uint8_t        = unsigned char;
+using uint16_t       = unsigned short;
+using uint32_t       = unsigned int;
+using uint64_t       = unsigned long long;
+using uint_fast8_t   = unsigned char;
+using uint_fast16_t  = unsigned short;
+using uint_fast32_t  = unsigned int;
+using uint_fast64_t  = unsigned long long;
+using uint_least8_t  = unsigned char;
+using uint_least16_t = unsigned short;
+using uint_least32_t = unsigned int;
+using uint_least64_t = unsigned long long;
+using uintmax_t      = unsigned long long;
+using uintptr_t      = unsigned long long;
+
+}; // namespace std
+
+#endif // _STD_CSTDINT_HPP
