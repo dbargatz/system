d310cb003b02bde5ef50a4d7d097294f8603e702
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-07-26 22:35:34 +0000

    Clean up assert(); remove __func__ (not a macro).

--- src/std/cassert.hpp
+++ src/std/cassert.hpp
@@ -7,19 +7,20 @@
 
 #define STRINGIFY_INTERNAL(x) #x
 #define STRINGIFY(x) STRINGIFY_INTERNAL(x)
+#define _ASSERT_MSG(cond) __FILE__ ":" STRINGIFY(__LINE__) ": Assertion '" #cond "' failed."
 
 // TODO: this implementation is implicitly reliant on the PANIC() definition,
 //       as well as nastily hardcoded. Fix this.
-#define assert(cond) \
-    if(!(cond)) {                                                     \
+#define assert(cond)                                                \
+    if(!(cond)) {                                                   \
         asm volatile("ud2       # panic_data::instruction\n"        \
                     ".word %c0  # panic_data::type       \n"        \
                     ".quad %c1  # panic_data::filename   \n"        \
                     ".word %c2  # panic_data::lineNum    \n"        \
                     ".quad %c3  # panic_data::msg        \n"        \
                     : /* No outputs */                              \
-                    : "i" (0xF2B5), "i" (__FILE__),         \
-                    "i" (__LINE__), "i" ("" __FILE__ ":" STRINGIFY(__LINE__) ": " STRINGIFY(__func__) ": Assertion '" #cond "' failed.") \
+                    : "i" (0xF2B5), "i" (__FILE__), "i" (__LINE__), \
+                      "i" (_ASSERT_MSG(cond))                       \
                     : /* No clobbers */);                           \
     }
 #endif // NDEBUG
