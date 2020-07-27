85e42730608f8f7816cc29d56178d6931545b75c
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-07-27 00:17:04 +0000

    Add logger to binary class.

--- src/loader/binary.hpp
+++ src/loader/binary.hpp
@@ -1,11 +1,17 @@
 #ifndef _LOADER_BINARY_HPP
 #define _LOADER_BINARY_HPP
 
+#include "../logging/logger.hpp"
+
 namespace loader {
 
 class binary {
 public:
+    binary(logging::logger& in_logger) : _log(in_logger) {}
     bool init(const void * in_start_addr, const void * in_end_addr);
+
+private:
+    logging::logger& _log;
 };
 
 }; //namespace loader
