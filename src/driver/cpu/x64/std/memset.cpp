bb498b81da25b00d0eb005962d0ea103b50ac6bc
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-07-28 02:18:35 +0000

    Remove old/unused x64 core driver stdlib files.

--- src/driver/cpu/x64/std/memset.cpp
+++
@@ -1,8 +0,0 @@
-// #include "memset.hpp"
-
-// void memset(void * inPtr, uint8_t inValue, size_t inNum) {
-//     // Simple, inefficient, but can be upgraded later.
-//     for(size_t i = 0; i < inNum; i++) {
-//         ((uint8_t *)inPtr)[i] = inValue;
-//     }
-// }
