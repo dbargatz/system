92e8ad8fbabedf12be061e3d660694406c9f5873
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-07-28 02:08:29 +0000

    Update formatting.

--- src/abi/cxxabi.h
+++ src/abi/cxxabi.h
@@ -7,16 +7,17 @@
  */
 namespace __cxxabiv1 {
 
-    /**
-     * @brief Panics if a pure virtual function is invoked (one without an
-     * implementation).
-     * 
-     * @note Satisfies Itanium C++ ABI, Section 3.2.6.
-     * @note Calls emitted by compiler for pure virtual functions; should not need
-     * to be invoked directly.
-     * @see https://stackoverflow.com/questions/920500
-     */
-    extern "C" [[noreturn]] void __cxa_pure_virtual();
-};
+/**
+ * @brief Panics if a pure virtual function is invoked (one without an
+ * implementation).
+ * 
+ * @note Satisfies Itanium C++ ABI, Section 3.2.6.
+ * @note Calls emitted by compiler for pure virtual functions; should not need
+ * to be invoked directly.
+ * @see https://stackoverflow.com/questions/920500
+ */
+extern "C" [[noreturn]] void __cxa_pure_virtual();
+
+}; // namespace __cxxabiv1
 
 #endif // __ABI_CXXABI_H
