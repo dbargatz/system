db5aad92598ca135f17789c85a4cfd6ad8d09206
Dylan Bargatze <dbargatz@users.noreply.github.com>
2020-08-30 01:56:42 +0000

    Add get_cpl function to determine if in ring 0/3.

--- src/driver/cpu/x64/core.cpp
+++ src/driver/cpu/x64/core.cpp
@@ -32,6 +32,17 @@ typedef void(*jump_usermode_fn)(void (*)(void));
 extern void* jump_usermode;
 extern void* syscall_handler;
 
+inline std::uint16_t get_cpl() {
+    std::uint16_t cs_selector = UINT16_MAX;
+    asm volatile("mov %%cs, %0" : "=m"(cs_selector));
+
+    // CS should only ever be 0x08 (Ring 0) or 0x2B (Ring 3); if not, this is a problem.
+    assert((0x08 == cs_selector) || (0x2B == cs_selector));
+
+    // Return Ring 0 for 0x08 and Ring 3 for 0x2B, which are the only options from the assert() above.
+    return (0x08 == cs_selector ? 0 : 3);
+}
+
 inline void syscall(const std::uint8_t in_id) {
     asm volatile("xor %%rdi, %%rdi; movb %0, %%dil; syscall" : : "m"(in_id));
 }
@@ -42,9 +53,9 @@ void usermode_fn() {
     auto log = log_injector.create<logging::logger&>();
     log.add_backend(&uart_log);
 
-    log.info(u8"Hello from usermode! Making syscall.");
+    log.info(u8"Hello from usermode (CPL: {})! Making syscall.", get_cpl());
     syscall(0x80);
-    log.info(u8"Back in usermode! Entering busy loop.");
+    log.info(u8"Back in usermode (CPL: {})! Entering busy loop.", get_cpl());
     while(true) {}
 }
 
@@ -96,7 +107,7 @@ void core::dispatch_interrupt(const void * in_frame_ptr) {
 }
 
 void core::dispatch_syscall(const std::uint8_t in_syscall_id) {
-    _log.info(u8"Syscall {} was invoked!", in_syscall_id);
+    _log.info(u8"Hello from supervisor mode (CPL: {})! Syscall {} was invoked.", get_cpl(), in_syscall_id);
 }
 
 void core::panic_handler(interrupt_frame& in_frame) {
