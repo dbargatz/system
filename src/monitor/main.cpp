38622da9c4d94cb09e0802160fe7747230cb4ffc
Dylan Bargatze <dbargatz@users.noreply.github.com>
2021-08-26 13:50:45 +0000

    Have monitor binary perform syscall.

--- src/monitor/main.cpp
+++ src/monitor/main.cpp
@@ -1,10 +1,12 @@
-void main() {
-    asm volatile("int $0x80");
+#include <cstdint>
+
+inline void syscall(const std::uint8_t in_id) {
+    asm volatile("xor %%rdi, %%rdi; movb %0, %%dil; syscall" : : "m"(in_id));
 }
 
 // Have to define because there is no nice libc that does this for us; see
 // https://web.archive.org/web/20200421041419/http://dbp-consulting.com/tutorials/debugging/linuxProgramStartup.html
 // for more info on what _start() does.
 extern "C" void _start() {
-    main();
+    syscall(0x22);
 }
