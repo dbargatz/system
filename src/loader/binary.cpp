f7fab1c6680918b6134fa1c10ff64beda4cea7c2
Dylan Bargatze <dbargatz@users.noreply.github.com>
2021-08-26 22:21:52 +0000

    Start extracting ELF info.

--- src/loader/binary.cpp
+++ src/loader/binary.cpp
@@ -4,6 +4,12 @@
 
 #include "__elf.hpp"
 
+void loader::binary::dump() {
+    _log.info(u8"\t\tStart Address: {:#016X}", (std::uint64_t)_elf_start_addr);
+    _log.info(u8"\t\tEnd Address  : {:#016X}", (std::uint64_t)_elf_end_addr);
+    _log.info(u8"\t\tLength       : {} bytes", _elf_length);
+}
+
 bool loader::binary::init(const void * in_start_addr, const void * in_end_addr) {
     auto file_hdr = (const struct Elf64_Ehdr *)in_start_addr;
     assert(file_hdr->e_ident[(std::uint8_t)Elf_Ident::EI_MAG0]       == 0x7F);
@@ -14,12 +20,15 @@ bool loader::binary::init(const void * in_start_addr, const void * in_end_addr) 
     assert(file_hdr->e_ident[(std::uint8_t)Elf_Ident::EI_DATA]       == 0x01); // Verify little-endian
     assert(file_hdr->e_ident[(std::uint8_t)Elf_Ident::EI_VERSION]    == 0x01); // Verify ELF spec version
     assert(file_hdr->e_ident[(std::uint8_t)Elf_Ident::EI_OSABI]      == 0x00); // Verify OS ABI is System V
-    // TODO: verify ABI version
+    assert(file_hdr->e_ident[(std::uint8_t)Elf_Ident::EI_ABIVERSION] == 0x00); // Verify System V ABI version
 
     assert(file_hdr->e_type    == 0x02); // Verify ET_EXEC
     assert(file_hdr->e_machine == 0x3E); // Verify machine is AMD64
     assert(file_hdr->e_version == 0x01); // Verify ELF spec version
-    _log.debug(u8"Hello from binary");
 
+    _elf_header = file_hdr;
+    _elf_start_addr = in_start_addr;
+    _elf_end_addr = in_end_addr;
+    _elf_length = (std::uint64_t)in_end_addr - (std::uint64_t)in_start_addr;
     return true;
 }
--- src/loader/binary.hpp
+++ src/loader/binary.hpp
@@ -1,6 +1,7 @@
 #ifndef _LOADER_BINARY_HPP
 #define _LOADER_BINARY_HPP
 
+#include <cstdint>
 #include "../logging/logger.hpp"
 
 namespace loader {
@@ -8,10 +9,15 @@ namespace loader {
 class binary {
 public:
     binary(logging::logger& in_logger) : _log(in_logger) {}
+    void dump();
     bool init(const void * in_start_addr, const void * in_end_addr);
 
 private:
     logging::logger& _log;
+    std::uint64_t _elf_length = 0;
+    const void * _elf_start_addr = 0;
+    const void * _elf_end_addr = 0;
+    const struct Elf64_Ehdr * _elf_header = nullptr;
 };
 
 }; //namespace loader
