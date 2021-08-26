5095f51d722b121b5ffed205b52443a80a0cbdf0
Dylan Bargatze <dbargatz@users.noreply.github.com>
2021-08-26 22:20:46 +0000

    Fix dereferencing of kernel heap labels.

--- src/driver/cpu/x64/memory/new.cpp
+++ src/driver/cpu/x64/memory/new.cpp
@@ -4,7 +4,7 @@
 
 extern void* kernel_heap_bottom;
 extern void* kernel_heap_top;
-std::uint8_t* cur_heap_ptr = (std::uint8_t *)kernel_heap_bottom;
+std::uint8_t* cur_heap_ptr = (std::uint8_t *)&kernel_heap_bottom;
 
 void* operator new(std::size_t size) {
     return ::operator new(size, sizeof(std::max_align_t));
@@ -14,12 +14,14 @@ void* operator new(std::size_t size, std::align_val_t alignment) {
     std::uint8_t * retval = cur_heap_ptr;
     std::uint8_t * new_ptr = cur_heap_ptr + size;
     new_ptr += ((std::uint64_t)new_ptr % alignment);
-    assert(new_ptr < kernel_heap_top);
+    assert(new_ptr >= (std::uint8_t *)&kernel_heap_bottom);
+    assert(new_ptr < (std::uint8_t *)&kernel_heap_top);
+    cur_heap_ptr = new_ptr;
     return retval;
 }
 
 void operator delete(void* ptr, std::size_t size, std::align_val_t alignment) noexcept {
-    assert(ptr >= kernel_heap_bottom);
-    assert(ptr < kernel_heap_top);
+    assert(ptr >= &kernel_heap_bottom);
+    assert(ptr < &kernel_heap_top);
     return;    
 }
