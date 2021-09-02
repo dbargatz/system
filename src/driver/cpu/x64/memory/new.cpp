e532b439407915b520895e9bd7953fc5db4f1478
Dylan Bargatze <dbargatz@users.noreply.github.com>
2021-09-02 02:00:17 +0000

    Fix incorrect aligned size calculation.

--- src/driver/cpu/x64/memory/new.cpp
+++ src/driver/cpu/x64/memory/new.cpp
@@ -12,7 +12,7 @@ void* operator new(std::size_t count) {
 }
 
 void* operator new(std::size_t count, std::align_val_t al) {
-    auto aligned_size = ((std::uint64_t)(cur_heap_ptr + count) % (std::size_t)al);
+    auto aligned_size = count + ((std::uint64_t)(cur_heap_ptr + count) % (std::size_t)al);
     std::uint8_t * retval = cur_heap_ptr;
 
     cur_heap_ptr += aligned_size;
