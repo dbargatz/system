22115f4f0e3534d2d7433e9e5ec694607f22136b
Dylan Bargatze <dbargatz@users.noreply.github.com>
2021-09-02 01:39:55 +0000

    Fix std::align_val_t; implement aligned new().

--- src/driver/cpu/x64/memory/new.cpp
+++ src/driver/cpu/x64/memory/new.cpp
@@ -1,23 +1,34 @@
 #include <new>
 #include <cassert>
 #include <cstdint>
+#include <cstring>
 
 extern void* kernel_heap_bottom;
 extern void* kernel_heap_top;
 std::uint8_t* cur_heap_ptr = (std::uint8_t *)&kernel_heap_bottom;
 
-void* operator new(std::size_t size) {
-    return ::operator new(size, sizeof(std::max_align_t));
+void* operator new(std::size_t count) {
+    return ::operator new(count, std::align_val_t(alignof(std::max_align_t)));
 }
 
-void* operator new(std::size_t size, std::align_val_t alignment) {
+void* operator new(std::size_t count, std::align_val_t al) {
+    auto aligned_size = ((std::uint64_t)(cur_heap_ptr + count) % (std::size_t)al);
     std::uint8_t * retval = cur_heap_ptr;
-    std::uint8_t * new_ptr = cur_heap_ptr + size;
-    new_ptr += ((std::uint64_t)new_ptr % alignment);
-    assert(new_ptr >= (std::uint8_t *)&kernel_heap_bottom);
-    assert(new_ptr < (std::uint8_t *)&kernel_heap_top);
-    cur_heap_ptr = new_ptr;
+
+    cur_heap_ptr += aligned_size;
+    assert(cur_heap_ptr >= (std::uint8_t *)&kernel_heap_bottom);
+    assert(cur_heap_ptr < (std::uint8_t *)&kernel_heap_top);
+
+    std::memset(retval, 0, aligned_size);
     return retval;
+}
+
+void* operator new[](std::size_t count) {
+    return ::operator new(count, std::align_val_t(alignof(std::max_align_t)));
+}
+
+void* operator new[](std::size_t count, std::align_val_t al) {
+    return ::operator new(count, std::align_val_t(alignof(std::max_align_t)));
 }
 
 void operator delete(void* ptr, std::size_t size, std::align_val_t alignment) noexcept {
