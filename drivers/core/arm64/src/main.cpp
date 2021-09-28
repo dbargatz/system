bd09011302d5af728d7a529710590bb5b754dd56
Dylan Bargatze <dbargatz@users.noreply.github.com>
2021-09-28 15:45:11 +0000

    Write hello world to the mini-UART.

--- drivers/core/arm64/src/main.cpp
+++ drivers/core/arm64/src/main.cpp
@@ -1,5 +1,8 @@
 #include <cstdint>
 
-extern "C" int core_entry(std::uint64_t dtb_ptr32, std::uint64_t x1, std::uint64_t x2, std::uint64_t x3) {
-    return -1;
+#include "console/uart.hpp"
+
+extern "C" void core_entry(std::uint64_t in_proc_id) {
+    core::arm64::console::uart log;
+    log.write("Hello, world!\n");
 }
