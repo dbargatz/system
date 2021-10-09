7fff01ac823dbc7a21adea2ebb4900e5fbe29fc6
Dylan Bargatze <dbargatz@users.noreply.github.com>
2021-10-09 20:30:44 +0000

    Make the core new/delete implementations platform-independent; implement raspi3b abort().

--- drivers/core/src/libcxx/new.cpp
+++ drivers/core/src/libcxx/new.cpp
@@ -3,11 +3,11 @@
 #include <cstdint>
 #include <cstring>
 
-std::uint8_t _early_heap[65535] = {0};
+static std::uint8_t _early_heap[65535] = {0};
 
-std::uint8_t* _early_heap_bottom = (std::uint8_t *)&_early_heap;
-std::uint8_t* _early_heap_top = _early_heap_bottom + 65535;
-std::uint8_t* cur_heap_ptr = _early_heap_bottom;
+static std::uint8_t* _early_heap_bottom = (std::uint8_t *)&_early_heap;
+static std::uint8_t* _early_heap_top = _early_heap_bottom + 65535;
+static std::uint8_t* cur_heap_ptr = _early_heap_bottom;
 
 void* operator new(std::size_t count) {
     return ::operator new(count, std::align_val_t(alignof(std::max_align_t)));
@@ -18,8 +18,8 @@ void* operator new(std::size_t count, std::align_val_t al) {
     std::uint8_t * retval = cur_heap_ptr;
 
     cur_heap_ptr += aligned_size;
-    assert(cur_heap_ptr >= (std::uint8_t *)&_early_heap_bottom);
-    assert(cur_heap_ptr < (std::uint8_t *)&_early_heap_top);
+    assert(cur_heap_ptr >= _early_heap_bottom);
+    assert(cur_heap_ptr < _early_heap_top);
 
     std::memset(retval, 0, aligned_size);
     return retval;
@@ -34,13 +34,13 @@ void* operator new[](std::size_t count, std::align_val_t al) {
 }
 
 void operator delete(void* ptr) noexcept {
-    assert(ptr >= &_early_heap_bottom);
-    assert(ptr < &_early_heap_top);
+    assert(ptr >= _early_heap_bottom);
+    assert(ptr < _early_heap_top);
     return;    
 }
 
 void operator delete(void* ptr, std::size_t size, std::align_val_t alignment) noexcept {
-    assert(ptr >= &_early_heap_bottom);
-    assert(ptr < &_early_heap_top);
+    assert(ptr >= _early_heap_bottom);
+    assert(ptr < _early_heap_top);
     return;    
 }
---
+++ drivers/core/src/platform/raspi3b/libcxx/abort.cpp
@@ -0,0 +1,18 @@
+#include <cstdlib>
+
+// #define STRINGIFY_INTERNAL(x) #x
+// #define STRINGIFY(x) STRINGIFY_INTERNAL(x)
+// #define _ASSERT_MSG(cond) __FILE__ ":" STRINGIFY(__LINE__) ": Assertion '" #cond "' failed."
+
+void std::abort(void) {
+    asm volatile("brk 0xF2B6");
+    // asm volatile("ud2        # panic_data::instruction\n"        \
+    //              ".word %c0  # panic_data::type       \n"        \
+    //              ".quad %c1  # panic_data::filename   \n"        \
+    //              ".word %c2  # panic_data::lineNum    \n"        \
+    //              ".quad %c3  # panic_data::msg        \n"        \
+    //              : /* No outputs */                              \
+    //              : "i" (0xF2B6), "i" (__FILE__), "i" (__LINE__), \
+    //              : "i" ("abort called")                          \
+    //              : /* No clobbers */);
+}
