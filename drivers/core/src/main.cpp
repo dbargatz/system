a37cd38ebc3b9f60439a83e81b57584f3e49e0a1
Dylan Bargatze <dbargatz@users.noreply.github.com>
2021-10-09 22:30:58 +0000

    Indicate that core_entry and std::abort are [[noreturn]].

--- drivers/core/src/main.cpp
+++ drivers/core/src/main.cpp
@@ -4,7 +4,7 @@
 #include "console/console.hpp"
 #include "platform.hpp"
 
-extern "C" void core_entry(std::uint64_t in_proc_id, const void* in_boot_info) {
+[[noreturn]] extern "C" void core_entry(std::uint64_t in_proc_id, const void* in_boot_info) {
     core::console::console log;
     log.info("Starting core driver for {} on processor {:X}", PLATFORM_NAME, in_proc_id);
     log.info("  Boot Info: {:016X}", in_boot_info);
--- drivers/core/src/platform/raspi3b/libcxx/abort.cpp
+++ drivers/core/src/platform/raspi3b/libcxx/abort.cpp
@@ -1,18 +1,10 @@
 #include <cstdlib>
 
-// #define STRINGIFY_INTERNAL(x) #x
-// #define STRINGIFY(x) STRINGIFY_INTERNAL(x)
-// #define _ASSERT_MSG(cond) __FILE__ ":" STRINGIFY(__LINE__) ": Assertion '" #cond "' failed."
-
-void std::abort(void) {
+[[noreturn]] void std::abort(void) {
+    // Invoking the "brk" (software breakpoint) instruction on aarch64 causes a Breakpoint
+    // Exception, which is handled by the platfom-specific interrupt handling system.
     asm volatile("brk 0xF2B6");
-    // asm volatile("ud2        # panic_data::instruction\n"        \
-    //              ".word %c0  # panic_data::type       \n"        \
-    //              ".quad %c1  # panic_data::filename   \n"        \
-    //              ".word %c2  # panic_data::lineNum    \n"        \
-    //              ".quad %c3  # panic_data::msg        \n"        \
-    //              : /* No outputs */                              \
-    //              : "i" (0xF2B6), "i" (__FILE__), "i" (__LINE__), \
-    //              : "i" ("abort called")                          \
-    //              : /* No clobbers */);
+
+    // This will never be reached, but indicates to the compiler that this function never returns.
+    while(true) {}
 }
