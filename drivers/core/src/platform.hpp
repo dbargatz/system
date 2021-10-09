43eda8e5c5fe3ea2ac53b6f93ad0e9bfa377e308
Dylan Bargatze <dbargatz@users.noreply.github.com>
2021-10-09 20:51:04 +0000

    Set PLATFORM_NAME constant for use with logging.

--- drivers/core/src/platform.hpp
+++ drivers/core/src/platform.hpp
@@ -1,7 +1,9 @@
 #if PLATFORM == qemu-pc
   #include "platform/qemu_pc/platform.hpp"
+  constexpr static const char8_t* PLATFORM_NAME = u8"QEMU PC";
 #elif PLATFORM == raspi3b
   #include "platform/raspi3b/platform.hpp"
+  constexpr static const char8_t* PLATFORM_NAME = u8"Raspberry Pi Model 3B";
 #else
   #error PLATFORM compile-time define was not specified or was invalid
 #endif
