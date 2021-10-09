441efb05d7ef7bd984ce0bc01edf27a2b9cbb675
Dylan Bargatze <dbargatz@users.noreply.github.com>
2021-10-09 20:38:57 +0000

    Remove newline, add abort().

--- drivers/core/src/main.cpp
+++ drivers/core/src/main.cpp
@@ -1,9 +1,11 @@
 #include <cstdint>
-#include <string>
+#include <cstdlib>
 
 #include "console/console.hpp"
 
 extern "C" void core_entry(std::uint64_t in_proc_id) {
     core::console::console log;
-    log.debug("Hello, world!\n");
+    log.debug("Hello, world!");
+    log.error("Aborting!");
+    std::abort();
 }
