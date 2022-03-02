78cc3bda6771a665d1193c9ee834533886057ce3
Dylan Bargatze <dbargatz@users.noreply.github.com>
2022-03-02 15:31:44 +0000

    Implement a pretty terrible heap.

--- drivers/core/src/memory/heap.cpp
+++ drivers/core/src/memory/heap.cpp
@@ -10,22 +10,48 @@ std::size_t core::memory::align_to(const std::align_val_t in_boundary, const std
 
 physical_addr_t heap::allocate(const std::size_t in_size, const std::align_val_t in_alignment) {
     auto aligned_size = align_to(in_alignment, in_size);
-    
-    if(aligned_size > _bytes_available) {
-        _log.warn("cannot allocate {} bytes in heap '{}', only {} bytes remain", aligned_size, (const char *)_name, _bytes_available);
+    auto cur_addr = (std::uint8_t *)_chunk_list;
+    auto max_addr = cur_addr + _bytes_total;
+
+    while(cur_addr < max_addr) {
+        auto cur_chunk = (struct heap_chunk *)cur_addr;
+        if(cur_chunk->free && cur_chunk->length >= aligned_size) {
+            break;
+        }
+        cur_addr = cur_addr + cur_chunk->length;
+    }
+
+    // We iterated the entire heap and couldn't find a chunk big enough for this allocation,
+    // either due to actual heap usage or fragmentation.
+    if(cur_addr >= max_addr) {
         return nullptr;
     }
 
-    // auto aligned_size = count + ((std::uint64_t)(cur_heap_ptr + count) % (std::size_t)al);
-    // std::uint8_t * retval = cur_heap_ptr;
+    // If the free chunk has enough space left for another heap chunk header plus the
+    // alignment size, then create a new free chunk immediately after the current chunk
+    // allocation and mark it as free. Otherwise, we'll consume the entire current chunk.
+    auto alloc_chunk = (struct heap_chunk *)cur_addr;
+    auto remaining_chunk_size = alloc_chunk->length - aligned_size;
+    if(remaining_chunk_size >= MIN_CHUNK_SIZE) {
+        auto new_free_chunk = (struct heap_chunk *)(cur_addr + aligned_size);
+        new_free_chunk->free = true;
+        new_free_chunk->length = remaining_chunk_size - sizeof(struct heap_chunk);
+        alloc_chunk->length = aligned_size;
 
-    // cur_heap_ptr += aligned_size;
-    // assert(cur_heap_ptr >= _early_heap_bottom);
-    // assert(cur_heap_ptr < _early_heap_top);
+        // We created a new heap chunk structure, so subtract that from the available
+        // bytes.
+        _bytes_available -= sizeof(struct heap_chunk);
+    }
 
-    // std::memset(retval, 0, aligned_size);
+    // Mark the chunk as used and clear out the actual heap memory.
+    alloc_chunk->free = false;
+    auto alloc_mem = cur_addr + sizeof(struct heap_chunk);
+    std::memset(alloc_mem, 0, aligned_size);
 
-    return nullptr;
+    // Update the heap statistics.
+    _bytes_available -= alloc_chunk->length;
+    _bytes_used += aligned_size;
+    return alloc_mem;
 }
 
 physical_addr_t heap::reserve(const physical_addr_t in_start, const std::size_t in_size, const std::align_val_t in_alignment) {
@@ -33,5 +59,18 @@ physical_addr_t heap::reserve(const physical_addr_t in_start, const std::size_t 
 }
 
 bool heap::deallocate(const physical_addr_t in_addr) {
-    return false;
+    // Sanity check - ensure the given address is somewhere within the heap boundaries.
+    assert(in_addr >= (physical_addr_t)_chunk_list);
+    assert(in_addr < ((physical_addr_t)_chunk_list + _bytes_total));
+
+    // Grab the associated heap chunk and ensure it's used (not already free).
+    auto chunk = (struct heap_chunk *)(in_addr - sizeof(struct heap_chunk));
+    assert(!chunk->free);
+
+    // TODO: Validate that this is a valid heap chunk (heap cookie?)
+    // TODO: Coalesce this chunk with any following free chunk.
+
+    // Mark the chunk as free again.
+    chunk->free = true;
+    return true;
 }
--- drivers/core/src/memory/heap.hpp
+++ drivers/core/src/memory/heap.hpp
@@ -4,7 +4,7 @@
 #include <cassert>
 #include <cstdint>
 #include <cstring>
-#include "../console/console.hpp"
+#include <new>
 #include "../platform.hpp"
 
 namespace core::memory {
@@ -12,37 +12,35 @@ namespace core::memory {
 typedef std::uint8_t * physical_addr_t;
 typedef std::uint8_t * virtual_addr_t;
 
-constexpr std::align_val_t DEFAULT_ALIGNMENT = (std::align_val_t)16;
-
 std::size_t align_to(const std::align_val_t in_boundary, const std::size_t in_num);
 
 struct alignas(16) heap_chunk {
-    std::uint8_t * next;
-    std::uint8_t * prev;
+    bool free;
+    std::size_t length;
 } __attribute__((packed));
+
+constexpr std::align_val_t DEFAULT_ALIGNMENT = (std::align_val_t)16;
+constexpr std::size_t MIN_CHUNK_SIZE = (std::size_t)DEFAULT_ALIGNMENT + sizeof(struct heap_chunk);
 
 class heap {
 private:
     char _name[16];
-    core::console::console& _log;
-    struct heap_chunk * _start;
-    struct heap_chunk * _next_free;
+    struct heap_chunk * _chunk_list;
     std::size_t _bytes_total;
     std::size_t _bytes_available;
     std::size_t _bytes_used;
 
 public:
-    heap(const char in_name[16], core::console::console& in_log, physical_addr_t in_initial_frame, std::size_t in_num_frames) : _log(in_log) {
+    heap(const char in_name[16], physical_addr_t in_initial_frame, std::size_t in_num_frames) {
         assert(((std::uintptr_t)in_initial_frame % PAGE_SIZE_BYTES) == 0);
 
         std::memcpy((void*)_name, in_name, 16);
-        _start = (struct heap_chunk *)in_initial_frame;
-        _start->next = nullptr;
-        _start->prev = nullptr;
-        _next_free = _start;
+        _chunk_list = (struct heap_chunk *)in_initial_frame;
+        _chunk_list->length = _bytes_total - sizeof(struct heap_chunk);
+        _chunk_list->free = true;
 
         _bytes_total = PAGE_SIZE_BYTES * in_num_frames;
-        _bytes_available = _bytes_total - sizeof(struct heap_chunk);
+        _bytes_available = _chunk_list->length;
         _bytes_used = 0;
     }
 
