6c02b6908af52c4bd569bdf1cd02ffe4d2c5d5c4
Dylan Bargatze <dbargatz@users.noreply.github.com>
2022-03-03 16:42:58 +0000

    Clean up and fix heap implementation.

--- drivers/core/src/memory/heap.cpp
+++ drivers/core/src/memory/heap.cpp
@@ -10,47 +10,55 @@ std::size_t core::memory::align_to(const std::align_val_t in_alignment, const st
 }
 
 physical_addr_t heap::allocate(const std::size_t in_size, const std::align_val_t in_alignment) {
+    // Align the allocation up to meet alignment requirements.
     auto aligned_size = align_to(in_alignment, in_size);
-    auto cur_addr = (std::uint8_t *)_chunk_list;
-    auto max_addr = cur_addr + _bytes_total;
+    auto cur_addr = _heap_start;
+    auto cur_chunk = (struct heap_chunk *)_heap_start;
 
-    while(cur_addr < max_addr) {
-        auto cur_chunk = (struct heap_chunk *)cur_addr;
+    // Walk through the heap chunks until we find a chunk large enough to fit the required
+    // aligned allocation, or until we reach the end of the heap. Note that this is a terrible,
+    // naiive "first fit" algorithm - it manages to be O(n) AND horribly space-inefficient!
+    while(cur_addr < _heap_end) {
         if(cur_chunk->free && cur_chunk->length >= aligned_size) {
             break;
         }
-        cur_addr = cur_addr + cur_chunk->length;
+        cur_addr += sizeof(*cur_chunk) + cur_chunk->length;
+        cur_chunk = (struct heap_chunk *)cur_addr;
     }
 
     // We iterated the entire heap and couldn't find a chunk big enough for this allocation,
     // either due to actual heap usage or fragmentation.
-    if(cur_addr >= max_addr) {
+    if(cur_addr >= _heap_end) {
         return nullptr;
     }
 
     // If the free chunk has enough space left for another heap chunk header plus the
     // alignment size, then create a new free chunk immediately after the current chunk
     // allocation and mark it as free. Otherwise, we'll consume the entire current chunk.
-    auto alloc_chunk = (struct heap_chunk *)cur_addr;
-    auto remaining_chunk_size = alloc_chunk->length - aligned_size;
+    auto remaining_chunk_size = cur_chunk->length - aligned_size;
     if(remaining_chunk_size >= MIN_CHUNK_SIZE) {
-        auto new_free_chunk = (struct heap_chunk *)(cur_addr + aligned_size);
+        // Create a new heap chunk for the memory from the current chunk we don't need.
+        auto new_free_chunk = (struct heap_chunk *)(cur_addr + sizeof(struct heap_chunk) + aligned_size);
         new_free_chunk->free = true;
         new_free_chunk->length = remaining_chunk_size - sizeof(struct heap_chunk);
-        alloc_chunk->length = aligned_size;
 
-        // We created a new heap chunk structure, so subtract that from the available
-        // bytes.
-        _bytes_available -= sizeof(struct heap_chunk);
+        // Update the overhead statistic - we created a new heap chunk, which isn't usable space.
+        _bytes_overhead += sizeof(struct heap_chunk);
+        cur_chunk->length = aligned_size;
+    } else {
+        // We don't create a new chunk, but the space remaining in the current chunk is technically
+        // overhead (aka internal fragmentation), because it's not usable space until the current
+        // chunk is freed. Count it as overhead!
+        _bytes_overhead += remaining_chunk_size;
     }
 
-    // Mark the chunk as used and clear out the actual heap memory.
-    alloc_chunk->free = false;
+    // Mark the current chunk as used and clear out the actual heap memory.
+    cur_chunk->free = false;
     auto alloc_mem = cur_addr + sizeof(struct heap_chunk);
     std::memset(alloc_mem, 0, aligned_size);
 
-    // Update the heap statistics.
-    _bytes_available -= alloc_chunk->length;
+    // Update the amount of memory actually used by the allocation; we count alignment bytes as
+    // part of the allocation, unlike chunk headers or internal fragmentation.
     _bytes_used += aligned_size;
     return alloc_mem;
 }
@@ -60,12 +68,12 @@ physical_addr_t heap::reserve(const physical_addr_t in_start, const std::size_t 
 }
 
 bool heap::deallocate(const physical_addr_t in_addr) {
-    // Sanity check - ensure the given address is somewhere within the heap boundaries.
-    assert(in_addr >= (physical_addr_t)_chunk_list);
-    assert(in_addr < ((physical_addr_t)_chunk_list + _bytes_total));
-
-    // Grab the associated heap chunk and ensure it's used (not already free).
-    auto chunk = (struct heap_chunk *)(in_addr - sizeof(struct heap_chunk));
+    // Sanity check - ensure the given address is somewhere within the heap boundaries, and is
+    // marked as free.
+    auto chunk_addr = in_addr - sizeof(struct heap_chunk);
+    auto chunk = (struct heap_chunk *)chunk_addr;
+    assert(chunk_addr >= _heap_start);
+    assert(in_addr < _heap_end);
     assert(!chunk->free);
 
     // TODO: Validate that this is a valid heap chunk (heap cookie?)
@@ -73,5 +81,10 @@ bool heap::deallocate(const physical_addr_t in_addr) {
 
     // Mark the chunk as free again.
     chunk->free = true;
+
+    // Update the heap statistics. Note that we're not updating _bytes_overhead, because we're
+    // not coalescing chunks yet.
+    _bytes_used -= chunk->length;
+
     return true;
 }
--- drivers/core/src/memory/heap.hpp
+++ drivers/core/src/memory/heap.hpp
@@ -24,24 +24,45 @@ constexpr std::size_t MIN_CHUNK_SIZE = (std::size_t)DEFAULT_ALIGNMENT + sizeof(s
 
 class heap {
 private:
+    // Name of heap for debugging purposes.
     char _name[16];
-    struct heap_chunk * _chunk_list;
-    std::size_t _bytes_total;
-    std::size_t _bytes_available;
+
+    // Physical start address of the contiguous memory range this heap can allocate/reserve from.
+    physical_addr_t _heap_start;
+
+    // Physical end address of the contiguous memory range this heap can allocate/reserve from.
+    physical_addr_t _heap_end;
+
+    // Number of heap bytes currently allocated/reserved to heap callers. Includes alignment bytes,
+    // as those bytes are technically usable by a caller without trashing heap_chunk structures.
     std::size_t _bytes_used;
+
+    // Number of heap bytes currently used for chunk headers and internal fragmentation, meaning
+    // they aren't usable by heap callers.
+    std::size_t _bytes_overhead;
 
 public:
     heap(const char in_name[16], physical_addr_t in_initial_frame, std::size_t in_num_frames) {
-        assert(((std::uintptr_t)in_initial_frame % PAGE_SIZE_BYTES) == 0);
+        // Sanity check - ensure the contiguous memory region given starts on a page boundary.
+        assert(((std::size_t)in_initial_frame % PAGE_SIZE_BYTES) == 0);
 
+        // Initialize the debug name and calculate the heap extents.
         std::memcpy((void*)_name, in_name, 16);
-        _chunk_list = (struct heap_chunk *)in_initial_frame;
-        _chunk_list->length = _bytes_total - sizeof(struct heap_chunk);
-        _chunk_list->free = true;
+        auto length = (PAGE_SIZE_BYTES * in_num_frames);
+        _heap_start = in_initial_frame;
+        _heap_end = _heap_start + length;
 
-        _bytes_total = PAGE_SIZE_BYTES * in_num_frames;
-        _bytes_available = _chunk_list->length;
+        // Allocate the first chunk at the start of the heap, marking it as free but subtracting
+        // the chunk header size from the length.
+        auto first_chunk = (struct heap_chunk *)_heap_start;
+        first_chunk->free = true;
+        first_chunk->length = length - sizeof(*first_chunk);
+
+        // Initialize the statistics vars - none of the bytes are used yet because nothing has
+        // asked the heap to allocate/reserve bytes yet, but we do have overhead bytes already,
+        // since we created a chunk header!
         _bytes_used = 0;
+        _bytes_overhead = sizeof(*first_chunk);
     }
 
     physical_addr_t allocate(const std::size_t in_size, const std::align_val_t in_alignment = DEFAULT_ALIGNMENT);
