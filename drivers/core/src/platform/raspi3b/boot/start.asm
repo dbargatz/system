232dd5a7be26898211923b6ab34c779af105bcaf
Dylan Bargatze <dbargatz@users.noreply.github.com>
2022-03-03 21:43:17 +0000

    Add missing MIT License from original src.

--- drivers/core/src/platform/raspi3b/boot/start.asm
+++ drivers/core/src/platform/raspi3b/boot/start.asm
@@ -1,3 +1,31 @@
+// Derived from bzt's "raspi3-tutorial" on GitHub; original file located at:
+// https://github.com/bztsrc/raspi3-tutorial/blob/dd48f11c74d6ea1fc013c206f99c9e23d643b6fa/01_bareminimum/start.S
+// Modifications: Copyright (C) 2022 Dylan Bargatze.
+// Original copyright/MIT License text follows:
+
+// Copyright (C) 2018 bzt (bztsrc@github)
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use, copy,
+// modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+
 // AArch64 mode
  
 // To keep this in the first portion of the binary.
@@ -9,7 +37,7 @@
  .org 0x80000
 _start:
     mrs    x0, mpidr_el1        
-    and    x0, x0,#0xFF        // Check processor id
+    and    x0, x0, #0xFF       // Check processor id
     cbz    x0, _bootstrap      // Hang for all non-primary CPU
     b      _hang
 
