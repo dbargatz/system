34d94b21ac920906adddcb0a9f5438c95ed9d55e
Dylan Bargatze <dbargatz@users.noreply.github.com>
2022-03-19 01:44:21 +0000

    Add a bunch more properties.

--- libs/libdevicetree/src/property.cpp
+++ libs/libdevicetree/src/property.cpp
@@ -37,16 +37,51 @@ std::string devicetree::property::format(std::size_t in_indent) const {
     if(_name == "model"sv ||
        _name == "status"sv ||
        _name == "name"sv ||
-       _name == "device_type"sv) {
+       _name == "device_type"sv ||
+       _name == "bootargs"sv ||
+       _name == "stdout-path"sv ||
+       _name == "stdin-path"sv ||
+       _name == "power-isa-version"sv ||
+       _name == "mmu-type"sv ||
+       _name == "label"sv
+    ) {
         auto value = get_value<std::string_view>();
         return std::format("{}{}: {}\n", indent, _name, value);
     } else if(
         _name == "phandle"sv ||
         _name == "#address-cells"sv ||
         _name == "#size-cells"sv ||
-        _name == "virtual-reg"sv
+        _name == "virtual-reg"sv ||
+        _name == "cache-op-block-size"sv ||
+        _name == "reservation-granule-size"sv ||
+        _name == "tlb-size"sv ||
+        _name == "tlb-sets"sv ||
+        _name == "d-tlb-size"sv ||
+        _name == "d-tlb-sets"sv ||
+        _name == "i-tlb-size"sv ||
+        _name == "i-tlb-sets"sv ||
+        _name == "cache-size"sv ||
+        _name == "cache-sets"sv ||
+        _name == "cache-block-size"sv ||
+        _name == "cache-line-size"sv ||
+        _name == "i-cache-size"sv ||
+        _name == "i-cache-sets"sv ||
+        _name == "i-cache-block-size"sv ||
+        _name == "i-cache-line-size"sv ||
+        _name == "d-cache-size"sv ||
+        _name == "d-cache-sets"sv ||
+        _name == "d-cache-block-size"sv ||
+        _name == "d-cache-line-size"sv ||
+        _name == "next-level-cache"sv ||
+        _name == "cache-level"sv ||
+        _name == "reg-shift"sv
     ) {
         auto value = get_value<std::uint32_t>();
+        return std::format("{}{}: {}\n", indent, _name, value);
+    } else if(
+        _name == "cpu-release-addr"sv
+    ) {
+        auto value = get_value<std::uint64_t>();
         return std::format("{}{}: {}\n", indent, _name, value);
     }
     return std::format("{}{}: {}\n", indent, _name, "???"sv);
