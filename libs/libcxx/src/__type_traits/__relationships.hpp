8fe19fd613168a56c424d5bc5fd5d1ef0151dbad
Dylan Bargatze <dbargatz@users.noreply.github.com>
2022-04-30 15:16:19 +0000

    Add std::is_same.

---
+++ libs/libcxx/src/__type_traits/__relationships.hpp
@@ -0,0 +1,62 @@
+/**
+ * @file
+ * @brief Templates to determine the relationship between two given types, as
+ * defined by C++20 (N4849 draft) 20.15.6.
+ * 
+ * @note Some comments and implementations in this file were based off of or
+ * taken directly from the descriptions on
+ * https://en.cppreference.com/w/cpp/header/type_traits and various subpages.
+ * All errors are mine, all credit is theirs.
+ */
+#ifndef _STD_TYPE_TRAITS_RELATIONSHIPS_HPP
+#define _STD_TYPE_TRAITS_RELATIONSHIPS_HPP
+
+namespace std {
+
+/**
+ * @brief If `T` and `U` are the same type (taking into account const/volatile
+ * qualifications), then `value` is `true`. Otherwise, `value` is `false`.
+ *
+ * @details
+ * Commutativity is satisfied; i.e. for any two types `T` and `U`, the
+ * following must hold:
+ *   is_same<T, U>::value == is_same<U, T>::value
+ * 
+ * @tparam T type with optional const/volatile qualifiers
+ * @tparam U type with optional const/volatile qualifiers
+ * @param value `true` if `T` and `U` are the same type, taking into account
+ * const/volatile qualifications; `false` otherwise
+ */
+template<typename T, typename U>
+struct is_same : std::false_type {};
+ 
+ /**
+ * @brief If `T` and `U` are the same type (taking into account const/volatile
+ * qualifications), then `value` is `true`. Otherwise, `value` is `false`.
+ *
+ * @details
+ * Commutativity is satisfied; i.e. for any two types `T` and `U`, the
+ * following must hold:
+ *   is_same<T, U>::value == is_same<U, T>::value
+ * 
+ * @tparam T type with optional const/volatile qualifiers
+ * @tparam U type with optional const/volatile qualifiers
+ * @param value `true` if `T` and `U` are the same type, taking into account
+ * const/volatile qualifications; `false` otherwise
+ */
+template<typename T>
+struct is_same<T, T> : std::true_type {};
+
+/**
+ * @brief `True` if `T` and `U` are the same type, taking into account const/
+ * volatile qualifications; otherwise, `false`.
+ * 
+ * @tparam T type with optional const/volatile qualifiers
+ * @tparam U type with optional const/volatile qualifiers
+ */
+template <typename T, typename U>
+inline constexpr bool is_same_v = is_same<T, U>::value;
+
+}; // namespace std
+
+#endif // _STD_TYPE_TRAITS_RELATIONSHIPS_HPP
