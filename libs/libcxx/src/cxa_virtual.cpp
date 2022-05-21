48f3923062833cb713a3714af3e1ecc8aedd8f2f
Dylan Bargatze <dbargatz@users.noreply.github.com>
2022-05-21 19:02:31 +0000

    Remove x86-specific instruction.

--- libs/libcxx/src/cxa_virtual.cpp
+++ libs/libcxx/src/cxa_virtual.cpp
@@ -3,9 +3,6 @@
 extern "C" void __cxa_pure_virtual() {
     // TODO: convert this to use PANIC() (or equivalent) once that's in user-mode.
 
-    // Halt should cause the program to crash. Loop so the compiler understands
-    // this will never return.
-    while(true) {
-        asm("hlt");
-    }
+    // Loop so the compiler understands this will never return.
+    while(true) { }
 }
