487d8c6908a614d4af9dc584538a2b556d91911a
Dylan Bargatze <dbargatz@users.noreply.github.com>
2022-05-21 15:33:44 +0000

    Start reimplementing mem mgr as page frame allocator.

--- drivers/core/src/memory/manager.cpp
+++ drivers/core/src/memory/manager.cpp
@@ -16,7 +16,29 @@ alignas(PAGE_SIZE_BYTES) static std::uint8_t _heap_bytes[_HEAP_SIZE_BYTES] = {0}
  * driver heap with the provided arguments. Prior to this constructor returning, no dynamic
  * memory allocation can be performed.
  */
-memory_manager::memory_manager() : _core_heap(core::memory::heap("core heap", _heap_bytes, _NUM_HEAP_FRAMES)) { }
+memory_manager::memory_manager() : _core_heap(core::memory::heap("core heap", _heap_bytes, _NUM_HEAP_FRAMES)) {
+    auto initial_value = page_frame_range {
+        .num_frames = (UINT64_MAX >> 12),
+        .reserved = 0,
+        .type = reservation_type::UNINITIALIZED,
+        .start_pfn = 0,
+    };
+    _page_frames.push_back(initial_value);
+}
+
+void memory_manager::register_pages(const physical_addr_t in_start, const std::size_t in_size, const reservation_type in_type) {
+    auto alignment = (std::size_t)in_start % PAGE_SIZE_BYTES;
+    assertm(alignment == 0, "start address of range is not aligned on page boundary");
+
+    auto num_frames = (in_size >> 12) + (in_size % PAGE_SIZE_BYTES == 0 ? 0 : 1);
+    auto value = page_frame_range {
+        .num_frames = num_frames,
+        .reserved = 0,
+        .type = in_type,
+        .start_pfn = (std::uint64_t)in_start >> 12,
+    };
+    _page_frames.push_back(value);
+}
 
 physical_addr_t memory_manager::core_allocate(const std::size_t in_size, const std::align_val_t in_alignment) {
     return _core_heap.allocate(in_size, in_alignment);
--- drivers/core/src/memory/manager.hpp
+++ drivers/core/src/memory/manager.hpp
@@ -4,14 +4,37 @@
 #include <cassert>
 #include <cstdint>
 #include <format>
+#include <list>
 #include "../console/console.hpp"
 #include "heap.hpp"
 
+
+// Level 1: 1 uint64_t = 64 level 2 = 4096k pages, space = 8 bytes
+// Level 2: 1 uint64_t = 64 level 3 = 256k pages,  space = 8 * 64 = 512 bytes
+// Level 3: 1 uint64_t = 64 level 4 = 4k pages,    space = 8 * 64 * 64 = 32768 bytes
+// Level 4: 1 uint64_t = 64 pages,                 space = 8 * 64 * 64 * 64 = 2097152 bytes
+
 namespace core::memory {
+
+enum reservation_type {
+    UNINITIALIZED = 0,
+    UNALLOCATED = 1,
+    ALLOCATED = 2,
+    RESERVED_CORE_DRIVER = 3,
+    RESERVED_DEVICE = 4,
+};
+
+struct page_frame_range {
+    std::size_t num_frames : 52;
+    std::size_t reserved : 20;
+    reservation_type type : 4;
+    std::uint64_t start_pfn : 52;
+} __attribute__((packed));
 
 class memory_manager {
 private:
     heap _core_heap;
+    std::list<struct page_frame_range> _page_frames;
 
 public:
     /**
@@ -24,8 +47,44 @@ public:
     physical_addr_t core_reserve(const physical_addr_t in_start, const std::size_t in_size, const std::align_val_t in_alignment = (const std::align_val_t)16);
     bool core_deallocate(const physical_addr_t in_addr);
 
+    void register_pages(const physical_addr_t in_start, const std::size_t in_size, const reservation_type in_type);
+
     auto format() const {
-        return std::format("Memory Manager at 0x{:016X}:\n{}", (physical_addr_t)this, _core_heap);
+        auto str = std::format(
+            "Memory Manager at 0x{:X}:\n"
+            "  Frame Map    : {} bytes\n"
+            "  Ranges       : {}\n",
+            (physical_addr_t)this,
+            sizeof(_page_frames),
+            _page_frames.size()
+        );
+
+        for(auto&& range : _page_frames) {
+            auto typestr = "";
+            switch(range.type) {
+                case reservation_type::UNINITIALIZED:
+                    typestr = "Uninitialized ";
+                    break;
+                case reservation_type::UNALLOCATED:
+                    typestr = "Unallocated   ";
+                    break;
+                case reservation_type::ALLOCATED:
+                    typestr = "Allocated     ";
+                    break;
+                case reservation_type::RESERVED_CORE_DRIVER:
+                    typestr = "Core Driver   ";
+                    break;
+                case reservation_type::RESERVED_DEVICE:
+                    typestr = "Device        ";
+                    break;
+            }
+            auto start = range.start_pfn << 12;
+            auto end = start + (range.num_frames << 12) - 1;
+            auto range_str = std::format("    {}: 0x{:X} - 0x{:X}\n", typestr, start, end);
+            str.append(range_str);
+        }
+
+        return str;
     }
 };
 
