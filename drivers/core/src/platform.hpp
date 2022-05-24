3a4ecd256f73723358453c87e9339bd45035f687
Dylan Bargatze <dbargatz@users.noreply.github.com>
2022-05-24 11:22:27 +0000

    Remove unused PLATFORM_NAME.

--- drivers/core/src/platform.hpp
+++ drivers/core/src/platform.hpp
@@ -3,10 +3,8 @@
 
 #ifdef PLATFORM_QEMU_PC
   #include "platform/qemu_pc/platform.hpp"
-  constexpr static const char * PLATFORM_NAME = "QEMU PC";
 #elif PLATFORM_RASPI3B
   #include "platform/raspi3b/platform.hpp"
-  constexpr static const char * PLATFORM_NAME = "Raspberry Pi Model 3B";
 #else
   #error PLATFORM compile-time define was not specified or was invalid
 #endif
